bc84b2f7004e8978d051627b252e186c095f294f
Gowler <GowlSec@proton.me>
2026-07-18 20:13:13 +0200

    Fix CORS and update frontend auth structure

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,11 @@ app.use(
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://gowlsec-production.up.railway.app"
+  ],
+  credentials: true
 }));
 
 
