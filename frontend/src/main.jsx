4d2a7342b449539ded8987d30ff0c1cabc5786ae
Gowler <GowlSec@proton.me>
2026-07-20 03:20:55 +0200

    Add Vercel Analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>
 );
